0f4501ca0a06b16d14ce4c01e6ea096b1086625e
IvonneNelson <IvonneNelson@users.noreply.github.com>
2020-05-29 09:39:22 -0600

    Update GitHubDemo.cpp

--- GitHubDemo/GitHubDemo.cpp
+++ GitHubDemo/GitHubDemo.cpp
@@ -1,17 +1,20 @@
-
+//TODO:  Not exactly a functional program.  Make sure your program runs before you upload it and invite me.  -25
+//TODO:  score 25/50
 #include <iostream>
-#ifndef _FUNCTIONS_H
-#define _FUNCTIONS_H
+//TODO:  does not belong here #ifndef _FUNCTIONS_H
+//TODO:  does not belong here#define _FUNCTIONS_H
 
-#endif
+//TODO:  does not belong here#endif
 
 int main()
 {
-    namespace cout << "Hello World!\n";
+    //TODO:  There is no namespace here.  
+   /* namespace*/ cout << "Hello World!\n";
 
 
-
-    retrun 0;
+//TODO:  return misspelled.
+   // retrun 0;
+    return 0;
 
 }
 
